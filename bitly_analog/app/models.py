9c7d714125f6580f12dbdc43a5ddade0d19bbbaa
vizonet <vizonet@bk.ru>
2021-07-06 22:29:34 +0700

    Добавлены комментарии.

--- bitly_analog/app/models.py
+++ bitly_analog/app/models.py
@@ -11,7 +11,7 @@ from django.contrib.sessions.models import Session
 
 class Owner(models.Model):
     """ Модель БД. Хранит пользователей и их настройки на основе ключа сеанса Django. """
-    session = models.ForeignKey(Session, on_delete=models.CASCADE)                   # связь с таблицой сеансов Session
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)                   # связь с таблицей сеансов Session
     url_ttl = models.PositiveSmallIntegerField('Время жизни правила (в сутках)', default=1)
     trows_on_page = models.PositiveSmallIntegerField('Число строк таблицы правил на странице', default=3)
 
@@ -28,7 +28,7 @@ class Url(models.Model):
     expire_date = models.DateField('Дата удаления правила')     
     str_limit = models.PositiveSmallIntegerField('Число первых символов отображения оригинального URL в методе __str__'
                                                  , default=40)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)                   # связь с таблицой пользователей Owner
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)                   # связь с таблицей пользователей Owner
 
     def to_json(self):
         """ Сведения об объекте модели в формате JSON. """
@@ -50,7 +50,7 @@ class Url(models.Model):
 
 class Collection(models.Model):
     """ Модель БД. Хранит связи пользователей c их правилами сокращения ссылок. """
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)                   # связь с таблицой пользователей Owner
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)                   # связь с таблицей пользователей Owner
     url = models.ForeignKey(Url, on_delete=models.CASCADE, null=True)            # связь с таблицей правил Url
 
     def __str__(self):
@@ -61,7 +61,7 @@ class Collection(models.Model):
 class Log(models.Model):
     """ Модель БД. Хранит статистику выполнения операций пользователями и приложением. """
     date_time = models.DateTimeField('Дата и время операции', default=timezone.now)
-    owner = models.ForeignKey(Owner, null=True, on_delete=models.SET_NULL)       # связь с таблицой пользователей Owner
+    owner = models.ForeignKey(Owner, null=True, on_delete=models.SET_NULL)       # связь с таблицей пользователей Owner
     process = models.CharField('Имя процесса', max_length=100)
     execute = models.TextField('Что выполнено')
 
